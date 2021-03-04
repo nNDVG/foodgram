272a4c34a4ef17a974cb0f50c88ed3b635ec8022
nNDVG <grishinkrd@yandex.ru>
2021-03-04 16:52:08 +0300

    redirect for deploy

--- foodgram/users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import User
-
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_filter = ('email', 'name')
-    empty_value_display = '-пусто-'
--- foodgram/users/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-
-class User(AbstractUser):
-    name = models.CharField(verbose_name='Имя', max_length=150, blank=True)
-    first_name = None
-    last_name = None
-    email = models.EmailField(verbose_name='Адрес электронной\nпочты', unique=True)
