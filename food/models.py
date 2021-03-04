18451aee42d3b4339780465da355631eaba9cb1b
nNDVG <grishinkrd@yandex.ru>
2021-03-05 00:03:24 +0300

    gunicorn

--- food/models.py
+++ food/models.py
@@ -25,7 +25,7 @@ class Ingredient(models.Model):
 
 
 class Recipe(models.Model):
-    pub_date = models.DateTimeField(auto_now_add=True, verbose_name='дата публикации')
+    pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, blank=True, null=True, related_name='recipe', verbose_name='Автор'
     )
