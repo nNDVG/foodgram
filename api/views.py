c9de7a786affa59b99bc6102c0e2e990767b08e5
nNDVG <grishinkrd@yandex.ru>
2021-03-06 22:29:05 +0300

    Second review fix

--- api/views.py
+++ api/views.py
@@ -2,63 +2,62 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views import View
-from food.models import Favorites, Follow, Ingredient, Recipe, ShoppingList
 from rest_framework.utils import json
+
+from food.models import Favorites, Follow, Ingredient, Recipe, ShoppingList
 from users.models import User
 
 
 class Favorite(LoginRequiredMixin, View):
-
+    """Add to favorites """
     def post(self, request):
         body = json.loads(request.body)
         recipe = get_object_or_404(Recipe, id=int(body['id']))
         user = request.user
         Favorites.objects.get_or_create(user=user, recipe=recipe)
         return JsonResponse({'success': True})
-
+    """Delete from favorites """
     def delete(self, request, recipe_id):
-        recipe = get_object_or_404(Recipe, id=recipe_id)
-        user = request.user
-        Favorites.objects.filter(user=user, recipe=recipe).delete()
+        Favorites.objects.filter(user=request.user, recipe__id=recipe_id).delete()
         return JsonResponse({'success': True})
 
 
 class Subscribe(LoginRequiredMixin, View):
-
+    """Subscribe for author"""
     def post(self, request):
         request_body = json.loads(request.body)
         author = get_object_or_404(User, id=int(request_body['id']))
         user = request.user
         Follow.objects.get_or_create(user=user, author=author)
         return JsonResponse({'success': True})
-
+    """Unsubscribe from the author"""
     def delete(self, request, author_id):
-        author = get_object_or_404(User, id=author_id)
-        user = request.user
-        Follow.objects.filter(user=user, author=author).delete()
+        Follow.objects.filter(user=request.user, author=author_id).delete()
         return JsonResponse({'success': True})
 
 
 class Purchase(LoginRequiredMixin, View):
-
+    """Add to bag"""
     def post(self, request):
         body = json.loads(request.body)
         recipe = get_object_or_404(Recipe, id=int(body['id']))
         user = request.user
         ShoppingList.objects.get_or_create(user=user, recipe=recipe)
         return JsonResponse({'success': True})
-
+    """Delete from bag"""
     def delete(self, request, recipe_id):
-        recipe = get_object_or_404(Recipe, id=recipe_id)
-        user = request.user
-        ShoppingList.objects.filter(user=user, recipe=recipe).delete()
+        ShoppingList.objects.filter(user=request.user, recipe__id=recipe_id).delete()
         return JsonResponse({'success': True})
 
 
 class Ingredients(LoginRequiredMixin, View):
-
+    """
+    This API is needed to get ingredients by matching
+    in the existing database when filling in the ingredient
+    name on the recipe creation / editing page.
+    """
     def get(self, request):
         query = request.GET.get('query', None)
         ingredients = Ingredient.objects.filter(
-            title__icontains=query).values('title', 'dimension').order_by('title')
+            title__istartswith=query).values('title', 'dimension').order_by('title')
         return JsonResponse(list(ingredients), safe=False)
